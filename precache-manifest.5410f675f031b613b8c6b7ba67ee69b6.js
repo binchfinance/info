1eadd77f0f2d8892d9d3cf38dbaa931e294c606f
binchfinance <74846841+binchfinance@users.noreply.github.com>
2020-11-23 08:22:37 +0900

    Add files via upload

---
+++ precache-manifest.5410f675f031b613b8c6b7ba67ee69b6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31e4bb3eec39718846b50f78703b9000",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f67947112c4c5f236707",
+    "url": "/static/js/2.274d0972.chunk.js"
+  },
+  {
+    "revision": "1bcf502cdba6b5e524b6436cdf7a8c7a",
+    "url": "/static/js/2.274d0972.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73190ee121d079aad91b",
+    "url": "/static/js/main.2e8bd35e.chunk.js"
+  },
+  {
+    "revision": "fce5bf0832b93e7c2ea7",
+    "url": "/static/js/runtime-main.1e26787f.js"
+  },
+  {
+    "revision": "73dabb371ddce7c640334fe0904dcddf",
+    "url": "/static/media/eth.73dabb37.png"
+  },
+  {
+    "revision": "f2e6469ce6c3a889ce3a4f33274ea79e",
+    "url": "/static/media/logo-white.f2e6469c.png"
+  },
+  {
+    "revision": "4b4c5aed1a632c59507afa5f4420430b",
+    "url": "/static/media/logo.4b4c5aed.png"
+  }
+]);
